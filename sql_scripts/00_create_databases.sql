7b6af3a94df59bb241c905eba23794fe7b7e0078
Sophia-15 <deletrasopa@gmail.com>
2025-11-24 21:15:09 -0300

    configuring trendz

---
+++ sql_scripts/00_create_databases.sql
@@ -0,0 +1,10 @@
+-- Create additional databases required by services
+-- This script runs first (00_) to ensure databases exist before other init scripts
+
+-- Create MLflow database
+CREATE DATABASE mlflow;
+
+-- Create Trendz database
+CREATE DATABASE trendz;
+
+-- Note: 'thingsboard' database is created by default via POSTGRES_DB env variable
